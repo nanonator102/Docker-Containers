f9fe7c696e6a038ad8e8e70bdcd0b25945474956
Zachary Moroney <thenanonator102@gmail.com>
2023-04-24 16:24:35 +1000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/container
+
+java -version
+whoami
+
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+${MODIFIED_STARTUP}
