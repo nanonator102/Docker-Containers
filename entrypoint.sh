f67c684670c6837efeb2039356580afeb5a508b5
Zachary Moroney <thenanonator102@gmail.com>
2023-04-24 17:16:13 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 cd /home/container
 
 java -version
+cat /etc/passwd
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
